4bbce540b533a845839ff7669b55e0e8632133a2
hojong6636 <hojong6636@gmail.com>
2018-05-17 22:28:03 +0900

    importing header menu icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,10 @@ export default function HeaderComponent() {
   return (
     <div className="header"> 
       <div className="nav">
-        <div className="profile-img"> 
-      
+        <div className="using-icons-default-style">
+          <span className="chief-mate chief-mate-menu" />
+        <div className="profile-img">    
+        </div>
         </div>
       </div>
     </div>
