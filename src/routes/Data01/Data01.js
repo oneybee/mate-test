f81454e8a7df3cb8a51a1516852373864d7cd54b
hojong6636 <hojong6636@gmail.com>
2018-05-18 18:11:40 +0900

    add pie chart on Data01

--- src/routes/Data01/Data01.js
+++ src/routes/Data01/Data01.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
 import './Data01.scss';
-
+import { PieChart, Pie, Sector } from 'Recharts';
 // import Heading1 from '../../../stories/Building blocks 🏠/Elements/Heading1'
 
 import InfoBanner from '../../components/InfoBanner'
 import Span from '../../../Items/Building blocks 🏠/Elements/Span'
 
+const data = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
+                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200}];
+                   
 
+
+
+                  
 class Data01Component extends Component {
   constructor() {
     super()
@@ -19,7 +25,17 @@ class Data01Component extends Component {
         <InfoBanner />
         <h1>Data 01</h1>
         {/* <Heading1 /> */}
-        <Span />
+          <PieChart width={400} height={400}>
+            <Pie 
+              data={data} 
+              cx={200} 
+              cy={200} 
+              innerRadius={60}
+              outerRadius={80} 
+              fill="#8884d8"
+              onMouseEnter={this.onPieEnter}
+            />
+          </PieChart>
       </div>
     )
   }
